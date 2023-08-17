eb78a9b3768f07b1cdca31c1fbf009f926a8fb14
GustiG <118075768+GustiG@users.noreply.github.com>
2023-08-17 07:16:29 +0300

    Add files via upload

---
+++ 004_miles_to_km_converter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int miles = 0;
+    cout << "Miles: " << '\n';
+    cin >> miles;
+    cout << miles << " miles equals to " << miles * 1.609 << " kilometers."; 
+    
+    return 0;
+}
