ee4e4753905fa9c1b8ee808c4456b7d7892bf9d2
GustiG <118075768+GustiG@users.noreply.github.com>
2023-08-18 06:53:32 +0300

    Add files via upload

---
+++ 006_float_operations_with_min_and_max.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double val1, val2;
+    cout << "enter two floating-point values: " << '\n';
+    cin >> val1 >> val2;
+    double big, little;
+
+    if (val1 < val2) {
+        cout << val1 << " < " << val2 << '\n';
+        big = val2;
+        little = val1;
+    } else if (val2 < val1) {
+        cout << val2 << " < " << val1 << '\n'; 
+        big = val1;
+        little = val2;
+    } else {
+        cout << val1 << " & " << val2 << " are equal.\n";
+        big = val1, little = val2;
+    }
+    
+    cout << val1 << " + " <<  val2 << " = " << val1 + val2 << '\n';
+    cout << big << " - " << little << " = " << big - little << '\n';
+    cout << val1 << " * " << val2 << " = "<< val1 * val2 << '\n';
+    cout << big << " / " << little << " = " << big / little << '\n';
+
+    return 0;
+}
