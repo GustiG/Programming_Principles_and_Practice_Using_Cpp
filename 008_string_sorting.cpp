54a5c29a06d9e3586cb8b7dcb0f87811cf864a17
GustiG <118075768+GustiG@users.noreply.github.com>
2023-08-19 11:01:41 +0300

    Add files via upload

---
+++ 008_string_sorting.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    string a, b, c;
+    cout << "input three words: ";
+    cin >> a >> b >> c;
+
+    string unu, doi, trei;
+ if (a < b && a < c) {
+        unu = a;
+        if (b < c) {
+            doi = b;
+            trei = c;
+        } else {
+            doi = c;
+            trei = b;
+        }
+    } else if (b < a && b < c) {
+        unu = b;
+        if (a < c) {
+            doi = a;
+            trei = c;
+        } else {
+            doi = c;
+            trei = a;
+        }     
+    } else if (c < a && c < b) {
+        unu = c;
+        if (a < b) {
+            doi = a;
+            trei = b;
+        } else {
+            doi = b;
+            trei = a;
+        }
+    }
+
+    cout << "Here they are sorted: " << unu << ", " << doi << ", " << trei << "." << '\n'; 
+
+    return 0;
+
+}
